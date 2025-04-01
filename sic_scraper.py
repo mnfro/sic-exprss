4ca35b22bf90516b30dd5f350064c6ef1721b31b
Manf <63849808+mnfro@users.noreply.github.com>
2025-04-01 12:22:01 +0200

    Create sic_scraper.py

---
+++ sic_scraper.py
@@ -0,0 +1,31 @@
+import requests
+import os
+
+TOKEN = os.environ['TOKEN']
+CHAT_ID = os.environ['CHAT_ID']
+TL_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+def sendMessageToBot(message):
+   try:
+      requests.post(TL_URL, json={'chat_id': CHAT_ID, 'text': message})
+   except Exception as e:
+      print(e)
+
+def check_text_in_page(url, text_to_find):
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        if text_to_find in response.text:
+            sendMessageToBot("WEBSITE STILL UNDER MAINTENANCE")
+            return True
+        else:
+            sendMessageToBot("TICKET RELEASEE")
+            return False
+    
+    except requests.exceptions.RequestException as e:
+        print(f'Error: {e}')
+        return False
+
+url = "https://www.siciliaexpress.eu"
+text_to_find = "Coming soon"
+check_text_in_page(url, text_to_find)
